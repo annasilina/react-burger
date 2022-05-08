c4f2d0af55458792c5d748fbe073c06fe4e98d42
Anna Silina <kalemn@yandex.ru>
2022-05-08 17:53:53 +0300

    finished AppHeader

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -6,25 +6,25 @@ import {BurgerIcon, Logo, ListIcon, ProfileIcon} from '@ya.praktikum/react-devel
 function AppHeader() {
   return (
     <header className={appHeaderStyles.header}>
-      <nav className={appHeaderStyles.nav}>
+      <nav className="pt-4 pb-4">
         <ul className={appHeaderStyles.menuList}>
           <li className={appHeaderStyles.menuItem}>
             <BurgerIcon type={"primary"} />
-            <p>Конструктор</p>
+            <p className="text text_type_main-default pl-2 pt-4 pb-4 pr-5">Конструктор</p>
           </li>
           <li className={appHeaderStyles.menuItem}>
             <ListIcon type={"secondary"} />
-            <p>Лента заказов</p>
+            <p className="text text_type_main-default text_color_inactive pl-2 pt-4 pb-4 pr-5">Лента заказов</p>
           </li>
         </ul>
-        <div className="appHeaderStyles.logo">
-          <Logo />
-        </div>
-        <div className={appHeaderStyles.profile}>
-          <ProfileIcon type={"secondary"} />
-          <p>Личный кабинет</p>
-        </div>
       </nav>
+      <div className={appHeaderStyles.logo}>
+        <Logo />
+      </div>
+      <div className={appHeaderStyles.profile}>
+        <ProfileIcon type={"secondary"} />
+        <p className="text text_type_main-default text_color_inactive pl-2 pt-4 pb-4 pr-5">Личный кабинет</p>
+      </div>
     </header>
   );
 }
