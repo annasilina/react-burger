ef2e6c4d11c271d2c263c6cde41f17fbc844505c
Anna Silina <kalemn@yandex.ru>
2022-08-02 20:06:27 +0300

    fix render for ordercost

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './burger-constructor.module.css';
@@ -10,9 +10,9 @@ const BurgerConstructor = ({ setModalVisibility }) => {
 	const bunSelected = useSelector(state => state.burger.bunSelected);
 	const ingredientsSelected = useSelector(state => state.burger.ingredientsSelected)
 
-	const orderCost = (currentIngredients, bun) => {
-		return currentIngredients.reduce((prevValue, ingredient) => {return prevValue + ingredient.price}, bun.price * 2);
-	}
+	const orderCost = useMemo(() => {
+		return ingredientsSelected.reduce((prevValue, ingredient) => {return prevValue + ingredient.price}, bunSelected.price * 2);
+	}, [ingredientsSelected, bunSelected]);
 
 	const handleButtonClick = () => setModalVisibility(ingredientsSelected);
 
@@ -33,9 +33,12 @@ const BurgerConstructor = ({ setModalVisibility }) => {
 			</div>
 			<div className={`${styles.order} mt-10 mr-4`}>
 				<div className={`${styles.orderCost} mr-10`}>
-					<p className="text text_type_digits-medium mr-2">
-						{orderCost(ingredientsSelected, bunSelected)}
-					</p>
+					{
+						orderCost &&
+						<p className="text text_type_digits-medium mr-2">
+							{orderCost}
+						</p>
+					}
 					<CurrencyIcon type={'primary'} />
 				</div>
 				<Button type={'primary'} size={'large'} onClick={handleButtonClick}>
