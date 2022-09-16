9dd29daec4bdbd3f042cf726b7f516f88d294cfe
annasilina <92690224+annasilina@users.noreply.github.com>
2022-09-16 21:40:33 +0300

    fix dispatch at home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,17 +1,23 @@
+import React, {useCallback, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import {DndProvider} from 'react-dnd';
 import {HTML5Backend} from 'react-dnd-html5-backend';
-import styles from './home.module.css';
+
 import BurgerIngredients from '../../components/burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../../components/burger-constructor/burger-constructor';
 import Modal from '../../components/modal/modal';
 import OrderDetails from '../../components/order-details/order-details';
-import React, {useCallback, useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {getIngredients, RESET_SELECTED_INGREDIENTS} from '../../services/actions/burger-ingredients';
+
+import {RESET_SELECTED_INGREDIENTS} from '../../services/actions/burger-ingredients';
 import {createOrder, RESET_ORDER_DETAILS} from '../../services/actions/order-details';
 import {CONSTRUCTOR_RESET} from '../../services/actions/constructor';
 
+import styles from './home.module.css';
+
 const Home = () => {
+	const dispatch = useDispatch();
+	const [isOrderDetailsOpen, setIsOrderDetailsOpened] = useState(false);
+
 	const { ingredients, ingredientsIsLoading, ingredientsHasError } = useSelector((state) => ({
 		ingredients: state.ingredientsData.ingredients,
 		ingredientsIsLoading: state.ingredientsData.ingredientsIsLoading,
@@ -23,17 +29,6 @@ const Home = () => {
 		orderIsLoading: state.orderData.orderIsLoading,
 		orderHasError: state.orderData.orderHasError
 	}));
-
-	const dispatch = useDispatch();
-
-	const [isOrderDetailsOpen, setIsOrderDetailsOpened] = useState(false);
-
-	useEffect(
-		() => {
-			dispatch(getIngredients());
-		},
-		[dispatch]
-	);
 
 	const handleOrderDetailsOpen = useCallback((orderDetails) => {
 		dispatch(createOrder(orderDetails))
