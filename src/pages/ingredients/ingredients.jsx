96f9e6aa8dbef0a5aaec6aab428b9102c5025be0
annasilina <92690224+annasilina@users.noreply.github.com>
2022-09-16 20:56:42 +0300

    add ingredientPage: work and style

--- src/pages/ingredients/ingredients.jsx
+++ src/pages/ingredients/ingredients.jsx
@@ -1,12 +1,17 @@
 import IngredientDetails from '../../components/ingredient-details/ingredient-details';
+import styles from './ingredients.module.css'
+import React from 'react';
 
-const Ingredients = () => {
-
+const IngredientsPage = () => {
 	return (
-		<main>
-			<IngredientDetails />
-		</main>
+		<>
+			<main className={styles.main}>
+				<h1 className='text text_type_main-large'>Детали ингредиента</h1>
+				<IngredientDetails />
+			</main>
+		</>
+
 	)
 }
 
-export default Ingredients;
+export default IngredientsPage;
