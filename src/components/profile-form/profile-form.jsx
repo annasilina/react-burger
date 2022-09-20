6de46e923a2bc2c3056af72d78e87ba720e557bc
annasilina <92690224+annasilina@users.noreply.github.com>
2022-09-20 16:44:28 +0300

    fix opacity for save/cancel buttons

--- src/components/profile-form/profile-form.jsx
+++ src/components/profile-form/profile-form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Input} from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './profile-form.module.css';
 import {useForm} from '../../utils/hooks';
@@ -7,6 +7,7 @@ import {setUserData} from '../../services/actions/auth';
 import ErrorMessage from '../error-message/error-message';
 
 const ProfileForm = () => {
+	const [visible, setVisible] = useState(false);
 	const authData = useSelector(state => state.authData);
 	const dispatch = useDispatch();
 	const {values, setValues, handleFormChange} = useForm({
@@ -14,6 +15,12 @@ const ProfileForm = () => {
 		email: authData.user.email || '',
 		password: ''
 	})
+
+	useEffect(() => {
+		if (values.name === authData.user.name && values.email === authData.user.email && values.password === '') {
+			setVisible(false);
+		} else setVisible(true);
+	}, [values]);
 
 	const handleFormSubmit = (evt) => {
 		evt.preventDefault();
@@ -25,6 +32,7 @@ const ProfileForm = () => {
 		}
 
 		dispatch(setUserData(formValues));
+		setVisible(false);
 	}
 
 	const handleFormReset = (evt) => {
@@ -71,7 +79,7 @@ const ProfileForm = () => {
 			{authData.isUserDataFailed &&
 				<ErrorMessage errorMessage={authData.userDataErrorMessage} />
 			}
-			<div className={styles.buttonsContainer}>
+			<div className={styles.buttonsContainer} style={{opacity: visible ? 1 : 0}}>
 				<Button
 					type={'secondary'}
 					size={'medium'}
