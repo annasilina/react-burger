a54e4285d3dc83a15fe9b2af335e60ca2c073ea4
annasilina <92690224+annasilina@users.noreply.github.com>
2022-09-20 04:24:24 +0300

    fix access token cookie lifetime

--- src/utils/token.js
+++ src/utils/token.js
@@ -4,6 +4,6 @@ export const setTokenData = (data) => {
 	const accessToken = data.accessToken.split('Bearer ')[1];
 	const refreshToken = data.refreshToken;
 
-	setCookie('accessToken', accessToken, {expires: 1200, path: '/'})
+	setCookie('accessToken', accessToken, {expires: 1140, path: '/'})
 	localStorage.setItem('refreshToken', refreshToken);
 }
