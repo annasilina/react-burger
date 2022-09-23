865e8467b888f96301bd765665db6d6669facffc
annasilina <92690224+annasilina@users.noreply.github.com>
2022-09-23 18:16:31 +0300

    add test constants for orders feed add draft page and components for orders feed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,7 +8,9 @@ export const links = {
 	ingredients: '/ingredients',
 	ingredient: '/ingredients/:id',
 	ordersFeed: '/feed',
+	order: '/feed/:id',
 	userOrdersFeed: '/profile/orders',
+	userOrder: '/profile/orders/:id',
 };
 
 export const errors = {
@@ -16,3 +18,110 @@ export const errors = {
 	incorrectValues: 'email or password are incorrect',
 	incorrectToken: ['jwt malformed'],
 };
+
+
+// тестовые данные для верстки ленты заказов
+// тестовый объект ответа ленты заказов
+const ordersFeed = {
+	success: true,
+	orders: [
+		{
+			ingredients: [
+				'60d3b41abdacab0026a733c6',
+				'60d3b41abdacab0026a733cb',
+				'60d3b41abdacab0026a733d0',
+				'60d3b41abdacab0026a733cb',
+			],
+			"_id": "1",
+			"status": "done",
+			"number": "35444",
+			"createdAt": "2021-06-23T14:43:22.587Z",
+			"updatedAt": "2021-06-23T14:43:22.603Z"
+		},
+		{
+			ingredients: [
+				'60d3b41abdacab0026a733c6',
+				'60d3b41abdacab0026a733cb',
+				'60d3b41abdacab0026a733d1',
+				'60d3b41abdacab0026a733d0',
+				'60d3b41abdacab0026a733cb',
+			],
+			"_id": "2",
+			"status": "done",
+			"number": "35445",
+			"createdAt": "2021-06-23T14:44:22.587Z",
+			"updatedAt": "2021-06-23T14:44:22.603Z"
+		},
+		{
+			ingredients: [
+				'60d3b41abdacab0026a733c6',
+				'60d3b41abdacab0026a733cb',
+				'60d3b41abdacab0026a733d0',
+				'60d3b41abdacab0026a733d1',
+				'60d3b41abdacab0026a733cb',
+			],
+			"_id": "3",
+			"status": "done",
+			"number": "35446",
+			"createdAt": "2021-06-23T14:45:22.587Z",
+			"updatedAt": "2021-06-23T14:45:22.603Z"
+		},
+		{
+			ingredients: [
+				'60d3b41abdacab0026a733c6',
+				'60d3b41abdacab0026a733d1',
+				'60d3b41abdacab0026a733cb',
+				'60d3b41abdacab0026a733d0',
+				'60d3b41abdacab0026a733cb',
+			],
+			"_id": "4",
+			"status": "done",
+			"number": "35447",
+			"createdAt": "2021-06-23T14:46:22.587Z",
+			"updatedAt": "2021-06-23T14:46:22.603Z"
+		},
+		{
+			ingredients: [
+				'60d3b41abdacab0026a733c6',
+				'60d3b41abdacab0026a733d4',
+				'60d3b41abdacab0026a733d0',
+				'60d3b41abdacab0026a733d3',
+				'60d3b41abdacab0026a733cb',
+				'60d3b41abdacab0026a733d0',
+				'60d3b41abdacab0026a733cb',
+			],
+			"_id": "5",
+			"status": "done",
+			"number": "35448",
+			"createdAt": "2021-06-23T14:47:22.587Z",
+			"updatedAt": "2021-06-23T14:47:22.603Z"
+		},
+		{
+			ingredients: [
+				'60d3b41abdacab0026a733c6',
+				'60d3b41abdacab0026a733cb',
+				'60d3b41abdacab0026a733d0',
+				'60d3b41abdacab0026a733d3',
+				'60d3b41abdacab0026a733d4',
+				'60d3b41abdacab0026a733d0',
+				'60d3b41abdacab0026a733d3',
+			],
+			"_id": "6",
+			"status": "done",
+			"number": "35449",
+			"createdAt": "2021-06-23T14:48:22.587Z",
+			"updatedAt": "2021-06-23T14:48:22.603Z"
+		},
+	],
+	total: 28752,
+	totalToday: 6
+}
+
+// тестовый объект для сборки экрана готовности заказов
+export const ordersBoard = {
+	orders_done: [35448, 35445, 35446, 35447, 35448, 35449],
+	orders_in_pro: [35448, 35445, 35446, 35447, 35448, 35449],
+	total_orders: [28752],
+	total_today_orders: [6]
+}
+
