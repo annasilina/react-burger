5026ab48099775c3aabb8f3df47812730c0eb224
annasilina <92690224+annasilina@users.noreply.github.com>
2022-09-25 13:53:58 +0300

    fix preloader at feed

--- src/pages/feed/feed.jsx
+++ src/pages/feed/feed.jsx
@@ -4,35 +4,31 @@ import {useDispatch, useSelector} from 'react-redux';
 import Preloader from '../../components/preloader/preloader';
 import {useEffect} from 'react';
 import FeedBoard from '../../components/feed-board/feed-board';
-import {wsConnectionStart} from '../../services/actions/webSocket';
+import {wsConnectionClose, wsConnectionStart} from '../../services/actions/webSocket';
 
 const FeedPage = () => {
-	const ingredientsData = useSelector(state =>  state.ingredientsData);
 	const orderData = useSelector(state => state.wsData);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
 		dispatch(wsConnectionStart())
-	}, [])
 
-	return (
+		return () => {
+			dispatch(wsConnectionClose())
+		}
+	}, [dispatch])
+
+	return orderData.orders.length === 0 ? (
+		<Preloader type='loader' />
+		) : (
 		<>
-			{!orderData.wsConnected && ingredientsData.ingredientsIsLoading && !orderData.orders.length && <Preloader type='loader'/>}
-			{orderData.err && ingredientsData.ingredientsHasError && <Preloader type='error'/>}
-			{orderData.wsConnected &&
-				!orderData.error &&
-				!!orderData.orders.length &&
-				!ingredientsData.ingredientsIsLoading &&
-				!ingredientsData.ingredientsHasError && (
-					<section className={styles.main}>
-						<h1 className='text text_type_main-large pt-10 pb-5'>Лента заказов</h1>
-						<section className={styles.container} >
-							<FeedList orders={orderData.orders} />
-							<FeedBoard />
-						</section>
-					</section>
-				)
-			}
+			<section className={styles.main}>
+				<h1 className='text text_type_main-large pt-10 pb-5'>Лента заказов</h1>
+				<section className={styles.container} >
+					<FeedList orders={orderData.orders} />
+					<FeedBoard />
+				</section>
+			</section>
 		</>
 	)
 }
