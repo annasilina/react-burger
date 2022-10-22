3b142ce23259ca72adb492d29e0a1c4436a29cec
annasilina <92690224+annasilina@users.noreply.github.com>
2022-10-22 20:34:15 +0300

    delete comments in calcOrderCost.ts

--- src/order/calcOrderCost.ts
+++ src/order/calcOrderCost.ts
@@ -1,22 +1,6 @@
 import {TIngredient} from "../types/data";
 
 export const calcOrderCost = (ingredientsInOrder: Array<TIngredient>): number => {
-	// const bunInOrder = ingredientsInOrder.filter(ingredient => ingredient.type === 'bun')[0];
-	// const otherIngredients = ingredientsInOrder.filter(ingredient => ingredient.type !== 'bun');
-	//
-	// const bunCost = bunInOrder === undefined ? 0 : bunInOrder.price * 2;
-	//
-	// const otherCost = otherIngredients.length === 0
-	// 	? 0
-	// 	: otherIngredients.reduce((previousValue, currentValue) => previousValue + (currentValue.price * currentValue.count), 0);
-	//
-	// return bunCost + otherCost;
-
-	// const bunInOrder = ingredientsInOrder.filter(ingredient => ingredient.type === 'bun')[0];
-	// const otherIngredients = ingredientsInOrder.filter(ingredient => ingredient.type !== 'bun');
-	//
-	// const bunCost = bunInOrder === undefined ? 0 : bunInOrder.price * 2;
-
 	return ingredientsInOrder.length === 0
 		? 0
 		: ingredientsInOrder.reduce((previousValue, currentValue) => previousValue + (currentValue.price * currentValue.count), 0);
