b40457f53aae6384fd899fb2d403159372814bff
annasilina <92690224+annasilina@users.noreply.github.com>
2022-10-23 06:45:19 +0300

    refactor feed and feed details to tsx

--- src/pages/feed-details/feed-details.tsx
+++ src/pages/feed-details/feed-details.tsx
@@ -1,13 +1,17 @@
 import OrderFullInfo from '../../components/order-full-info/order-full-info';
 import styles from './feed-details.module.css'
-import {useEffect} from 'react';
+import {FC, useEffect} from 'react';
 import {wsConnectionClose, wsConnectionStart} from '../../services/actions/webSocket';
 import {wsConnectionCloseAuth, wsConnectionStartAuth} from '../../services/actions/webSocketAuth';
 import Preloader from '../../components/preloader/preloader';
 import {useTDispatch, useTSelector} from '../../services/hooks';
 
-const FeedDetailsPage = (props) => {
-	const {wsAuth} = props;
+interface IFeedDetailsPageProps {
+	wsAuth: boolean;
+}
+
+const FeedDetailsPage: FC<IFeedDetailsPageProps> = ({wsAuth}) => {
+
 	const dataAll = useTSelector(state => state.wsData);
 	const dataAuth = useTSelector(state => state.wsAuthData);
 
--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,12 +1,12 @@
 import FeedList from '../../components/feed-list/feed-list';
 import styles from './feed.module.css'
 import Preloader from '../../components/preloader/preloader';
-import {useEffect} from 'react';
+import {FC, useEffect} from 'react';
 import FeedBoard from '../../components/feed-board/feed-board';
 import {wsConnectionClose, wsConnectionStart} from '../../services/actions/webSocket';
 import {useTDispatch, useTSelector} from '../../services/hooks';
 
-const FeedPage = () => {
+const FeedPage: FC = () => {
 	const feedData = useTSelector(state => state.wsData);
 	const dispatch = useTDispatch();
 
